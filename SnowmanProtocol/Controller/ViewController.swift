6f1d53a3e524745e7ef3c7fdf6c826571a1bb5f7
branch1 <a.lapatin@icloud.com>
2018-12-05 13:59:37 +0300

    Delet gester recogniser. Add touchesBegan and hittest

--- SnowmanProtocol/Controller/ViewController.swift
+++ SnowmanProtocol/Controller/ViewController.swift
@@ -10,45 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
-   var isEyesOpen: Bool = true
-//    var eyesSwitch: Bool = true
-//    var eyeCount = 2
+    var isEyesOpen: Bool = true
+    var startPointGesture = CGPoint(x: 0, y: 0)
+    var respondView = UIView()
+    //    var eyesSwitch: Bool = true
+    //    var eyeCount = 2
+    
+    @IBOutlet weak var eyeView: UIImageView!
+    
+    @IBOutlet weak var imageViewSnowman: Snowman!
 
-    @IBOutlet weak var imageViewSnowman: Snowman! {
-        didSet{
-            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
-            imageViewSnowman.addGestureRecognizer(tapGestureRecognizer)
-        }
-    }
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    
-
-    @objc func handleTap (sender: UITapGestureRecognizer) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         
-//        let respondView = self.view.hitTest(<#T##point: CGPoint##CGPoint#>, with: <#T##UIEvent?#>)
-        
-        if let viewSnowman = sender.view as? Snowman {
-            isEyesOpen = !isEyesOpen
-            viewSnowman.eyesDelegat = self
-            viewSnowman.setNeedsDisplay()
+        if let touch = touches.first {
+            startPointGesture = touch.location(in: self.view)
+            
+            
+            if let respondView = self.view.hitTest(startPointGesture, with: nil) {
+                if respondView == eyeView {
+                    isEyesOpen = !isEyesOpen
+                    
+                    imageViewSnowman.eyesDelegat = self
+                    imageViewSnowman.setNeedsDisplay()
+                }
+            }
+            
         }
-        
     }
-    
-    
 
 }
 
 extension ViewController: EyesOpening {
-
+    
     func openEyes() -> Bool {
         return isEyesOpen
     }
